fff0b77fc1894e25cf45b8ee771fde8f3bf34003
ugetragul <ugetragul@gmail.com>
2018-10-22 21:59:26 +0100

    updated main

--- main.go
+++ main.go
@@ -45,7 +45,7 @@ func main() {
 func hello(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Serving request: %s", r.URL.Path)
 	host, _ := os.Hostname()
-	fmt.Fprintf(w, "Hello, Gul!\n")
+	fmt.Fprintf(w, "Vanakam, Gul!\n")
 	fmt.Fprintf(w, "Version: 1.0.0\n")
 	fmt.Fprintf(w, "Hostname: %s\n", host)
 }
